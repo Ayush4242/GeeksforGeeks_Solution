4580ba483462508810e4893f7ce5e756c822abe1
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2025-12-18 19:36:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        if(s1==s2){
+            return true;
+        }
+        if((s1+s1).find(s2)!=string::npos){
+            return true;
+        }
+        return false;
+        
+    }
+};
