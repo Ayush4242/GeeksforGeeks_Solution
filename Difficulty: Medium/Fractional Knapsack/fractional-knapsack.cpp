4ef56537205649e71f743971ac863bfbb72360b5
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2025-12-25 18:16:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -1,29 +1,26 @@
 class Solution {
   public:
     double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
-        vector<pair<int,int>>merge;
+        // code here
+        vector<pair<int,int>>ans;
         for(int i=0;i<val.size();i++){
-            merge.push_back({val[i],wt[i]});
+            ans.push_back({val[i],wt[i]});
         }
-        sort(merge.begin(),merge.end(),[](pair<int,int>&a,pair<int,int>&b){
-            double r1=double(a.first)/a.second;
-            double r2=double(b.first)/b.second;
-            return r1>r2;
+        sort(ans.begin(),ans.end(),[](pair<int,int>&a,pair<int,int>&b){
+            return (double)a.first / a.second > (double)b.first / b.second;
         });
-        double total=0.0;
-        for(int i=0;i<merge.size();i++){
-            int value=merge[i].first;
-            int weight=merge[i].second;
-            if(weight<capacity){
-                total+=value;
-                capacity-=weight;
-            }
-            else{
-                total+=((double)value/weight)*capacity;
+        double s=0,res=0;
+        for(int i=0;i<ans.size();i++){
+            if(s+ans[i].second>capacity){
+                double rem=capacity-s;
+                res+=(rem/ans[i].second)*ans[i].first;
                 break;
             }
+            else{
+                s+=ans[i].second;
+                res += ans[i].first;
+            }
         }
-        return total;
-        
+        return res;
     }
 };
