8a328384ba947e18f73fb4bcce37dfd377718ddf
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2025-12-31 13:26:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -7,18 +7,19 @@ class Solution {
             ans.push_back({val[i],wt[i]});
         }
         sort(ans.begin(),ans.end(),[](pair<int,int>&a,pair<int,int>&b){
-            return (double)a.first / a.second > (double)b.first / b.second;
+            return (double)a.first/(double)a.second>(double)b.first/(double)b.second;
         });
-        double s=0,res=0;
+        double res=0;
+        int s=0;
         for(int i=0;i<ans.size();i++){
             if(s+ans[i].second>capacity){
-                double rem=capacity-s;
-                res+=(rem/ans[i].second)*ans[i].first;
+                int rem=capacity-s;
+                res+=((double)rem/ans[i].second)*ans[i].first;
                 break;
             }
             else{
                 s+=ans[i].second;
-                res += ans[i].first;
+                res+=ans[i].first;
             }
         }
         return res;
