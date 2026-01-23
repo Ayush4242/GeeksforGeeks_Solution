9e4e11838241be2722277baba841a405547fd682
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-01-23 16:10:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Game with String/game-with-string.cpp
+++ Difficulty: Medium/Game with String/game-with-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
   public:
     int minValue(string &s, int k) {
-        unordered_map<int,int>mp;
-        priority_queue<int>pq;
+        unordered_map<char,int>mp;
         for(auto i:s){
             mp[i]++;
         }
+        priority_queue<int>pq;
         for(auto i:mp){
             pq.push(i.second);
         }
         while(!pq.empty() && k>0){
             int t=pq.top();
-            k--;
             t--;
+            k--;
             pq.pop();
             pq.push(t);
         }
         int sum=0;
         while(!pq.empty()){
             int t=pq.top();
-            sum+=(t*t);
+            sum+=t*t;
             pq.pop();
         }
         return sum;
