0961ec74187ea217a5cad6187eebd6bcb4cef3a0
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-02-12 13:49:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.cpp
+++ Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.cpp
@@ -1,23 +1,24 @@
 class Solution {
   public:
     string firstNonRepeating(string &s) {
-        unordered_map<char,int>mp;
         queue<char>q;
-        string str="";
+        unordered_map<char,int>mp;
+        string res="";
         for(auto i:s){
             mp[i]++;
             q.push(i);
-            while(!q.empty() && mp[q.front()]>1){
-                q.pop();
-            }
-            if(q.empty()){
-                str+="#";
-            }
-            else{
-                str+=q.front();
-            }
-        }
-        return str;
         
+        while(!q.empty() && mp[q.front()]>1){
+            q.pop();
+        }
+        if(q.empty()){
+            res+="#";
+        }
+        else{
+            res+=q.front();
+        }
+        }
+        
+        return res;
     }
 };
