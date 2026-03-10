816c3fb572780355dfe09fbe8db696c58b5c6ca9
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-03-10 22:33:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
+++ Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
@@ -2,8 +2,8 @@ class Solution {
   public:
     int findMinDiff(vector<int>& a, int m) {
         sort(a.begin(),a.end());
-        int n=a.size(),mini=INT_MAX;
-        for(int i=0;i+m-1<n;i++){
+        int mini=INT_MAX;
+        for(int i=0;(i+m-1)<a.size();i++){
             mini=min(mini,a[i+m-1]-a[i]);
         }
         return mini;
