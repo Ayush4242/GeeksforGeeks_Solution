991fe217afc01873306c141c7cbe18d4424e9649
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-03-10 22:54:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -3,26 +3,23 @@ class Solution {
     // Function to find the maximum number of meetings that can
     // be performed in a meeting room.
     int maxMeetings(vector<int>& start, vector<int>& end) {
-        // Your code here
-        if(start.size()==0){
-            return 0;
-        }
         vector<pair<int,int>>ans;
         for(int i=0;i<start.size();i++){
             ans.push_back({start[i],end[i]});
         }
-        sort(ans.begin(),ans.end(),[](pair<int,int>&a,pair<int,int>&b){
-            return a.second<b.second;
+        sort(ans.begin(),ans.end(),[](pair<int,int> &a,pair<int,int> &b){
+                return a.second<b.second;
             
         });
-        int c=1;
-        int free=ans[0].second;
+        int time=ans[0].second,c=1;
         for(int i=1;i<ans.size();i++){
-            if(ans[i].first>free){
+            if(ans[i].first>time){
                 c++;
-                free=ans[i].second;
+                time=ans[i].second;
             }
+            
         }
         return c;
+        
     }
 };
