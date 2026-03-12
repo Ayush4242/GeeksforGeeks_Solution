bc19d85fe5c321f22ce53f032da06c9689c66229
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-03-12 14:48:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -1,27 +1,29 @@
 class Solution {
   public:
     double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
-        // code here
         vector<pair<int,int>>ans;
         for(int i=0;i<val.size();i++){
             ans.push_back({val[i],wt[i]});
         }
         sort(ans.begin(),ans.end(),[](pair<int,int>&a,pair<int,int>&b){
-            return (double)a.first/(double)a.second>(double)b.first/(double)b.second;
+            return (double)a.first/a.second>(double)b.first/b.second;
         });
-        double res=0;
-        int s=0;
+        double s=0;
         for(int i=0;i<ans.size();i++){
-            if(s+ans[i].second>capacity){
-                int rem=capacity-s;
-                res+=((double)rem/ans[i].second)*ans[i].first;
-                break;
+            
+            if(capacity-ans[i].second>=0){
+                capacity-=ans[i].second;
+                s+=ans[i].first;
             }
             else{
-                s+=ans[i].second;
-                res+=ans[i].first;
+                s+=((double)ans[i].first/ans[i].second)*capacity;
+                break;
+                
             }
+            
         }
-        return res;
+        return s;
+        
+        
     }
 };
