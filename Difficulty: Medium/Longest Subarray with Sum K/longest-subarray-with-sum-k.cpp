cd2b999c80968ab232433df37cc61d42dd87c236
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-04-06 17:29:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -7,11 +7,14 @@ class Solution {
         for(int i=0;i<arr.size();i++){
             sum+=arr[i];
             if(mp.find(sum-k)!=mp.end()){
-                maxi=max(maxi,i-mp[sum-k]);
+                maxi=max(maxi,i-(mp[sum-k]));
             }
             if(mp.find(sum)==mp.end()){
                 mp[sum]=i;
             }
+            
+                
+            
         }
         return maxi;
         
