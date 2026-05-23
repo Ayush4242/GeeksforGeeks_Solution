4cfe7b5d72c7a9cecdf67b1e2c8340b9df82274b
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-05-23 12:41:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void rotateArr(vector<int>& arr, int d) {
+        int size=arr.size();
+        d=d%size;
+        reverse(arr.begin(),arr.begin()+d);
+        reverse(arr.begin()+d,arr.end());
+        reverse(arr.begin(),arr.end());
+        
+        
+    }
+};
