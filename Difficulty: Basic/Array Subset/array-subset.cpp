3261dedc5c85397d5dc20f049c4402f5bd4ee771
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-06-18 19:40:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Subset/array-subset.cpp
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -2,17 +2,26 @@ class Solution {
   public:
     // Function to check if b is a subset of a
     bool isSubset(vector<int> &a, vector<int> &b) {
-        unordered_map<int,int>mp;
-        // unordered_map<int,int>mp2;
-        for(auto i:a){
-            mp[i]++;
-        }
-        for(auto i:b){
-            if(mp[i]<=0){
-                return false;
+        // Your code here
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        
+        int size = a.size();
+        int size1 = b.size();
+        int p = 0;
+        int q = 0;
+        for(int i = 0; i<size;i++) {
+            if(a[i]==b[q]) {
+                q++;
+                p++;
             }
-            mp[i]--;
+            if(q==size1) {
+                break;
+            }
         }
-        return true;
+        if(p==size1) {
+            return true;
+        }
+        return false;
     }
 };
